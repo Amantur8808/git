d49eaaea0d6b356fc7e3409259a40d1eea86334a
Amantur <abolotbaev.00@gmail.com>
2021-09-16 17:17:53 +0600

    your message

---
+++ modules/homework/variables.tf
@@ -0,0 +1,28 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "ami" {
+  description = "AMI"
+  type = string
+  default = ""
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "user_data" {
+  type = any
+  
+}
+
+variable "cidr_blocks" {
+  default = "10.0.1.0/24"
+}
+
+variable "cidr_block" {
+  default = "0.0.0.0/0"
+}
+
+
