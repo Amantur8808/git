9da0261a71311f40d0934f4f141016815a970851
Amantur <89061638+Amantur8808@users.noreply.github.com>
2023-02-10 23:29:24 +0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+
+variable "ami" {
+  description = "AMI"
+  type = string
+  default = ""
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "user_data" {
+  type = any
+  
+}
+
+variable "cidr_blocks" {
+  default = "10.0.1.0/24"
+}
+
+variable "cidr_block" {
+  default = "0.0.0.0/0"
+}
